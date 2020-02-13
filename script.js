e2a225e90f928be471cea665122d2aa2d6954f7a
Antonykirunyu <kirunyuanthony@gmail.com>
2020-02-13 22:09:47 +0300

    changed entire page

--- script.js
+++ script.js
@@ -1,35 +1,108 @@
-function getName() {
-
-    var year = parseInt(document.getElementById("YYY").value);
-    var month =parseInt(document.getElementById("MMM").value);
-    var day = parseInt(document.getElementById("DDD").value);
-    var date = new Date(`${year}-${month-1}-${day}`);
-    var date = new Date(year, month-1, day);
-    var dob = date.getDay();
-    var male = document.getElementById("male");
-    var female = document.getElementById("female");
-
-    var Weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-
-
-    if (male.checked == false && female.checked == false) {
-        alert("Hey identify your gender");
+function validateform(){
+    var day = Number(document.getElementById("day-to-earth").value);
+    var month = Number(document.getElementById("month-to-earth").value);
+    var year = Number(document.getElementById("year").value);
+    var century = Number(document.getElementById("century").value);
+    var genderSelected = document.getElementsByName("gender");
+    function genderPicked() {
+        for (var gender of genderSelected) {
+            if (gender.checked){
+                return gender.value;
+            }
+        }
     }
-    else if (year == "" || year.length < 4 || year.length > 4){
-        alert("Hey you need to enter a valid year!");
+    var genderValue = genderPicked();
+    function dayValid () {
+        if (day < 1 || day > 31) {
+            alert("Invalid day")
+            return false;
+        } else {
+            return true;
+        }
     }
-    else if (month < 1 || month > 12 || month == 2 && day > 29 ){
-        alert("Hey you need to enter a valid month!");
+    function monthValid () {
+        if (month < 1 || month > 12) {
+            alert ("Invalid month");
+            return false;
+        } else {
+            return true;
+        }
     }
-    else if( day < 1 || day >31) {
-        alert("Hey you need to re-enter a valid day!");
-    }
-    else if (male.checked === true) {
-        alert("Hey, " + maleNames[dob] + " is your Akan name" + " since you were born on a " + Weekdays[dob]);
+  
+    var isDayValid = dayValid();
+    var isMonthValid = monthValid();
+
+    var dayIndex = Math.round(( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day) % 7);
+ var akanName;
+    if (genderValue === "male") {
+        switch(dayIndex) {
+                case 0:
+            akanName = "Kwasi";
+            alert("Your name is Kwasi");
+            break;
+            case 1:
+            akanName = "Kwadwo";
+            alert("Your name is Kwadwo");
+            break;
+            case 2:
+            akanName = "Kwabena";
+            alert("Your name is Kwabena");
+            break;
+            case 3:
+            akanName = "Kwaku";
+            alert("Your name is Kwaku");
+            break;
+            case 4:
+            akanName = "Yaw";
+            alert("Your name is Yaw");
+            break;
+            case 5:
+            akanName = "Kofi";
+            alert("Your name is Kofi");
+            break;
+            case 6:
+            akanName = "Kwame";
+            alert("Your name is Kwame");
+            break;
+            default:
+            akanName = "Please input the correct details!"
+            alert("Please input the correct details!");
+        }
+    } else if (genderValue = "female") {
+        switch(dayIndex) {
+                case 0:
+                akanName = "Akosua";
+                alert("Your name is Akosua");
+                break;
+                case 1:
+                akanName = "Adwoa";
+                alert("Your name is Adwoa");
+                break;
+                case 2:
+                akanName = "Abenaa";
+                alert("Your name is Abenaa");
+                break;
+                case 3:
+                akanName = "Akua";
+                alert("Your name is Akua");
+                break;
+                case 4:
+                akanName = "Yaa";
+                alert("Your name is Yaa");
+                break;
+                case 5:
+                akanName = "Afua";
+                alert("Your name is Afua");
+                break;
+                case 6:
+                akanName = "Ama";
+                alert("Your name is Ama");
+                break;
+                default:
+                akanName = "Please input the correct details!"
+                alert("Please input the correct details!");
+            }
     } else {
-        alert("Hey, " + femaleNames[dob] + " is your Akan name" +" since you were born on a " + Weekdays[dob]);
+                alert("Please try again with the correct data.");
     }
-
 }
